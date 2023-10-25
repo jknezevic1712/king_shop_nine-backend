7b895445d9f8e8f77913e9bd3349ed0ea539aafa
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-10-25 18:16:13 +0200

    Database code changes:
    Added function `FetchProducts` that fetches products from `Products` table and returns result as stringified JSON

--- king-shop-nine/database/product-actions.go
+++ king-shop-nine/database/product-actions.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"encoding/json"
 	"king-shop-nine/utils"
 	"log"
 )
@@ -28,3 +29,66 @@ func AddProduct(newProduct utils.Product) error {
 	conn.Close()
 	return nil
 }
+
+// Fetch all products
+//
+// @returns []Product
+func FetchProducts() string {
+	var products []utils.Product
+	conn := ConnectToDB()
+
+	rows, err := conn.Query(`SELECT * FROM "Products"`)
+	if err != nil {
+		log.Printf("FetchProducts: error executing query to the DB, %v\n", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var product utils.Product
+		if err = rows.Scan(&product.ID, &product.Title, &product.ShortDescription, &product.Description, &product.Category, &product.Subcategory, &product.Image, &product.DateAdded, &product.Rating.Rate, &product.Rating.Count); err != nil {
+			log.Printf("FetchProducts: error while fetching products, %v\n", err)
+		}
+
+		products = append(products, product)
+	}
+	if err = rows.Err(); err != nil {
+		log.Printf("FetchProducts: error while fetching products, %v\n", err)
+	}
+
+	marshalledRes, err := json.Marshal(products)
+	if err != nil {
+		log.Printf("FetchProducts: error while converting result to json, %v\n", err)
+	}
+	stringifiedJson := string(marshalledRes)
+
+	conn.Close()
+	return stringifiedJson
+}
+
+// Fetch product by ID
+//
+// @args productID of int64 type
+//
+// @returns Product
+// func FetchProductByID(productID int64) utils.Product {
+// 	var jsonData []byte
+// for rows.Next() {
+// 		if err := rows.Scan(&jsonData); err != nil {
+// 			log.Printf("FetchProducts: error while fetching products, %v\n", err)
+// 		}
+// 	}
+
+// 	var product utils.Product
+// 	conn := ConnectToDB()
+
+// 	row := conn.QueryRow(`SELECT * FROM "Products" WHERE id = $1`, productID)
+
+// 	if err := row.Scan(&product.ID, &product.Title, &product.ShortDescription, &product.Description, &product.Category, &product.Subcategory, &product.Image, &product.DateAdded, &product.Rating.Rate, &product.Rating.Count); err != nil {
+// 		log.Printf("FetchProductByID: error while fetching product with id %c, %v\n", productID, err)
+// 	}
+
+// 	log.Printf("FetchProductByID: fetched product => %v\n", product)
+// 	conn.Close()
+// 	return product
+// }
--- king-shop-nine/main.go
+++ king-shop-nine/main.go
@@ -3,7 +3,9 @@ package main
 import "king-shop-nine/database"
 
 func main() {
-	database.CreateEmptyTables()
-	database.PopulateUsersTableWithMockData()
-	database.PopulateProductsTableWithMockData()
+	// database.CreateEmptyTables()
+	// database.PopulateUsersTableWithMockData()
+	// database.PopulateProductsTableWithMockData()
+
+	database.FetchProducts()
 }
