11b6e88196c0facc21689db620d1455eee80894e
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-10-28 10:19:38 +0200

    Api package changes:
    Updated response messages to include details property that has error or nil as value so that the client can get better understanding of what went wrong

--- king-shop-nine/api/initializer.go
+++ king-shop-nine/api/initializer.go
@@ -20,7 +20,7 @@ func InitializeApi() {
 func getProducts(c *gin.Context) {
 	products, err := database.FetchProducts()
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "products could not be retrieved"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "products could not be retrieved", "details": err})
 	}
 
 	c.JSON(http.StatusOK, products)
@@ -33,12 +33,12 @@ func getProducts(c *gin.Context) {
 func getProductByID(c *gin.Context) {
 	productID, err := utils.StringToInt(c.Param("id"))
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "product id provided is invalid"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "product id provided is invalid", "details": err})
 	}
 
 	product, err := database.FetchProductByID(productID)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "product with the specified ID could not be retrieved"})
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "product with the specified ID could not be retrieved", "details": err})
 	}
 
 	c.JSON(http.StatusOK, product)
@@ -51,16 +51,16 @@ func postProduct(c *gin.Context) {
 
 	// Call BindJSON to bind the received JSON to newProduct
 	if err := c.ShouldBindJSON(&newProduct); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "product data is invalid"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "product data is invalid", "details": err})
 		return
 	}
 
 	if err := database.AddProduct(newProduct); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "product unsuccessfully created"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "product unsuccessfully created", "details": err})
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{"message": "product successfully created"})
+	c.JSON(http.StatusCreated, gin.H{"message": "product successfully created", "details": nil})
 
 	// curl http://localhost:8080/product --include --header "Content-Type: application/json" --request "POST" --data '{"id": 35,"title": "Old school jeans","price": 34.99,"shortDescription": "Be cool, be oldschool!","description": "Old school style jeans that return you back to the good ol` days!","category": "unisex","subcategory": "all","image": "https://i.ibb.co/ZYW3VTp/brown-brim.png","dateAdded": "1682267143000","rating":{"rate": 4.6,"count": 15}}'
 	// {"id": 35,"title": "Old school jeans","price": 34.99,"shortDescription": "Be cool, be oldschool!","description": "Old school style jeans that return you back to the good ol` days!","category": "unisex","subcategory": "all","image": "https://i.ibb.co/ZYW3VTp/brown-brim.png","dateAdded": "1682267143000","rating": {"rate": 4.6,"count": 15}}
