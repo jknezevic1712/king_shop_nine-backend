21edbfd0d03bf7537371b6cc573db484c110ce7f
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-10-28 18:26:36 +0200

    Database package changes:
    Updated functions to return error as well because of better client error explaining

--- king-shop-nine/database/user-actions.go
+++ king-shop-nine/database/user-actions.go
@@ -65,14 +65,15 @@ func SignUpUser(newUser utils.User) error {
 
 // Fetch all users
 //
-// @returns string
-func FetchUsers() string {
+// @returns []User, error
+func FetchUsers() ([]utils.User, error) {
 	users := []utils.User{}
 	conn := ConnectToDB()
 
 	rows, err := conn.Query(`SELECT * FROM "Users"`)
 	if err != nil {
 		log.Printf("FetchUsers: error executing query to the DB, %v\n", err)
+		return users, err
 	}
 
 	defer rows.Close()
@@ -80,12 +81,14 @@ func FetchUsers() string {
 	_, err = rows.Columns()
 	if err != nil {
 		log.Printf("FetchUsers: error while returning column names, %v\n", err)
+		return users, err
 	}
 
 	for rows.Next() {
 		user := utils.User{}
 		if err := rows.Scan(&user.ID, &user.Name, &user.Email, &user.AccountCreatedAt, &user.Image); err != nil {
 			log.Printf("FetchUsers: error while fetching users, %v\n", err)
+			return users, err
 		}
 
 		users = append(users, user)
@@ -93,18 +96,19 @@ func FetchUsers() string {
 
 	if err := rows.Err(); err != nil {
 		log.Printf("FetchUsers: error while fetching users, %v\n", err)
+		return users, err
 	}
 
 	conn.Close()
-	return utils.ToJSON(users)
+	return users, nil
 }
 
 // Fetch user by ID and optionally update user session if the `isLogin` argument is true
 //
 // @args userID string, isLogin bool
 //
-// @returns string
-func FetchUserByID(userID string, isLogin bool) string {
+// @returns User, error
+func FetchUserByID(userID string, isLogin bool) (utils.User, error) {
 	user := utils.User{}
 	conn := ConnectToDB()
 
@@ -112,6 +116,7 @@ func FetchUserByID(userID string, isLogin bool) string {
 
 	if err := row.Scan(&user.ID, &user.Name, &user.Email, &user.AccountCreatedAt, &user.Image); err != nil {
 		log.Printf("FetchUserByID: error while fetching user with id %v, %v\n", userID, err)
+		return user, err
 	}
 
 	if isLogin {
@@ -119,5 +124,5 @@ func FetchUserByID(userID string, isLogin bool) string {
 	}
 
 	conn.Close()
-	return utils.ToJSON(user)
+	return user, nil
 }
